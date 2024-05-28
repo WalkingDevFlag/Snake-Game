c016a687b68a2f9447e27d77e854ee80dd641795
Siddharth Panditrao <115323514+WalkingDevFlag@users.noreply.github.com>
2024-05-28 22:30:09 +0530

    Add files via upload

---
+++ Snake Game/config.py
@@ -0,0 +1,14 @@
+# config.py
+
+# Screen dimensions
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+
+# Block size
+BLOCK_SIZE = 20
+
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GRAY = (50, 50, 50)
